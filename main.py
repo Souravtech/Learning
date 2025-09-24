2199ced8065553af932ede02d262b906d69df519
Souravtech <dassourav521@gmail.com>
2025-09-24 23:02:25 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#input the name from the users
+name = str(input("Enter your name:"))
+age = int(input("Enter your age:"))
+gender = str(input("Enter your gender:"))
+
+#display the user
+print(f"Hello, {name} your age is {age} and your gender is {gender}")
